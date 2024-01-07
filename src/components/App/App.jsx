69207f3d70133dfcd233f49563e5ec4b79161628
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-01-07 20:41:34 +0200

    loading works

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,26 +4,42 @@ import '../../styles.css';
 import ImageGallery from 'components/ImageGallery';
 import PixabayApi from 'components/Api/Api';
 import Button from 'components/Button/Button';
-import Modal from 'components/Modal';
+import Spinner from 'components/Spinner';
+// import Modal from 'components/Modal';
 
 class App extends Component {
-  state = { gallery: [], searchQuery: '' };
+  state = {
+    gallery: [],
+    searchQuery: '',
+    isLoading: false,
+    showLoadMore: false,
+  };
+
   request = new PixabayApi();
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevState.searchQuery !== this.state.searchQuery) {
+      this.setState({ isLoading: true });
+
       try {
         this.setState({ gallery: [] });
         this.request.resetPage();
         const images = await this.request.getPhotos(this.state.searchQuery);
-        this.setState({ gallery: [...images.hits] });
+        if (images.lengts < this.request.per_page) {
+          this.setState({ showLoadMore: false });
+        }
+
+        this.setState({ gallery: [...images.hits], showLoadMore: false });
       } catch (error) {
         console.log(error);
+      } finally {
+        this.setState({ isLoading: false, showLoadMore: false });
       }
     }
   }
 
   handleLoadMoreButton = async () => {
+    this.setState({ isLoading: true });
     try {
       const images = await this.request.getPhotos(this.state.searchQuery);
 
@@ -32,6 +48,8 @@ class App extends Component {
       }));
     } catch (error) {
       console.log(error);
+    } finally {
+      this.setState({ isLoading: false });
     }
   };
 
@@ -44,7 +62,8 @@ class App extends Component {
       <div className="app">
         <Searchbar onSubmit={this.onSubmit} />
         <ImageGallery images={this.state.gallery} />
-        {this.state.gallery.length && (
+        <Spinner isLoading={this.state.isLoading} />
+        {this.state.showLoadMore && (
           <Button onClick={this.handleLoadMoreButton} />
         )}
       </div>
